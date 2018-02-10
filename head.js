a99f5f211fdf1df143bd2f6adc30ea794ba91aa4
Carl Törnqvist <calle.tornqvist@gmail.com>
2018-02-10 22:37:14 +0100

    Add favicons

---
+++ head.js
@@ -0,0 +1,17 @@
+const hyperstream = require('hyperstream')
+
+module.exports = head
+
+function head (str) {
+  return hyperstream({
+    head: {
+      _appendHtml: `
+        <link rel="apple-touch-icon" sizes="180x180" href="/assets/apple-touch-icon.png">
+        <link rel="icon" type="image/png" sizes="32x32" href="/assets/favicon-32x32.png">
+        <link rel="icon" type="image/png" sizes="16x16" href="/assets/favicon-16x16.png">
+        <link rel="mask-icon" href="/assets/safari-pinned-tab.svg" color="#dc143c">
+        <meta name="msapplication-TileColor" content="#ffffff">
+      `
+    }
+  })
+}
