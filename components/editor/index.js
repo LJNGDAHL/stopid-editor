dc2f76b4c816a62504d63c661619709d8f442123
Katarina Ljungdahl <katarina@folkungagatan54.se>
2018-02-10 14:02:14 +0100

    Add custom marker

--- components/editor/index.js
+++ components/editor/index.js
@@ -5,7 +5,24 @@ const css = require('sheetify')
 
 const prefix = css`
   :host:focus {
+    caret-color: transparent;
     outline: none;
+  }
+
+  :host .Marker {
+    animation: blink 1400ms steps(5, start) infinite;
+    content: "";
+    display: inline-block;
+    height: 2rem;
+    position: relative;
+    top: 0.4rem;
+    width: 0.2rem;
+  }
+
+  @keyframes blink {
+    to {
+      visibility: hidden;
+    }
   }
 `
 
@@ -18,12 +35,18 @@ module.exports = class Editor extends Nanocomponent {
     return true
   }
 
+
   createElement (state, emit) {
+    let char = 0
+
     return html`
-      <div onkeydown=${onkeydown} contenteditable="true" class="${prefix} ma3 f3 lh-copy sans-serif mw7 w-80 vh-100">
+      <div onkeydown=${onkeydown} contenteditable="true" class="${prefix} relative ma3 f3 lh-copy sans-serif mw7 w-80 vh-100">
         ${state.words.reduce((words, word) => {
+          char += word.text.length + 1
+
           return words.concat(html`<span>${word.text}</span>`, raw`&nbsp`)
         }, [])}
+        <div class="Marker bg-dark-red"></div>
       </div>
     `
 
